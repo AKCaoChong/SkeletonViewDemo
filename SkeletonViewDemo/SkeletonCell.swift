7a138d7ef9ac7bb3b3406efbc25e32c3891df833
caochong0621 <caochong0621@thundersoft.com>
2018-05-12 09:53:49 +0800

    code

---
+++ SkeletonViewDemo/SkeletonCell.swift
@@ -0,0 +1,24 @@
+//
+//  SkeletonCell.swift
+//  SkeletonViewDemo
+//
+//  Created by 曹冲 on 2018/5/2.
+//  Copyright © 2018年 曹冲. All rights reserved.
+//
+
+import UIKit
+
+class SkeletonCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
